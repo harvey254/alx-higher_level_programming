62cc03f0d27f838c1979b46da02b96d52aeb8b54
harvey254 <harveybill74@gmail.com>
2024-04-03 10:47:53 +0300

    lop

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Defines a class Square"""
+
 class Square:
-    """
-    an empty class Square that defines a square
-    
+    """An empty class Square that defines a square
     """
     pass
