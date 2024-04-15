5d7a95fbcb130defc3f3791a74cfb9247c5730de
harvey254 <harveybill74@gmail.com>
2024-04-15 21:06:08 +0300

    ml

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' class Square '''
+    def __init__(self, size):
+        ''' Argument:
+            @size: size of the square
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        ''' Return square description '''
+        return ("[Square] {}/{}".format(self.__size, self.__size))
