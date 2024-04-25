c9650253e807f8d52f70e5306857d61187ae3227
harvey254 <harveybill74@gmail.com>
2024-04-25 17:15:47 +0300

    cool

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
-""" class Base module """
-
+"""class Base module"""
 import json
 
 
 class Base:
-    """ base class
-    Attributes:
-        _nb_objects: number of objects created
-        id: id of object
+    """Class that defines properties of Base.
+
+     Attributes:
+        id (int): Identity of each instance.
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
-    """ Creates new instances 
-        args:
-            id: id of object
-    """
+        """Creates new instances of Base.
+
+        Args:
+            id (int, optional): Identity of each instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
      
-     @classmethod
-     def int_validator(self, name, value):
-         """ method validator
+    def int_validator(self, name, value):
+         """ method validator.
+         
          Arguments;
          @name: attribute to validate
          @value: value to validate
+         
          Returns:
          Type or Value error if is not an integer or
          if is not a positive number.
         """
-        if type(value) is not int:
+         if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
+         if value <= 0:
             raise ValueError('{} must be > 0'.format(name))
      
-     @classmethod
-     def integer_validator2(self, name, value):
+    def integer_validator2(self, name, value):
         """ method validator
+        
         Arguments;
         @name: attribute to validate
         @value: value to validate
+        
         Returns:
         Type or Value error if is not an integer or
         if is not a positive number.
@@ -56,8 +58,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns JSON string
+        
         args:
             list_dictionaries: list of dictionaries
+        
         return:
             return serialized list or empty list
         """
@@ -66,8 +70,10 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ json to string static method
+        
         args:
             json_string: json object string type
+        
         return:
             list of json strings
         """
@@ -79,8 +85,10 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """ writes JSON string to a file
+        
         args:
             list_objs: list of objects
+        
         return:
             na
         """
@@ -94,21 +102,24 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
          """ return instance with all attributes set
+        
         args:
             dictionary: double pointer
+        
         return:
             instance with set attribute
         """
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        if cls.__name__ == "Square":
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
+         if cls.__name__ == "Rectangle":
+             dummy = cls(1, 1)
+         if cls.__name__ == "Square":
+             dummy = cls(1)
+         dummy.update(**dictionary)
+         return dummy
 
     @classmethod
     def load_from_file(cls):
         """ Returns a list of instances
+        
         return:
             list of instance json string
         """
